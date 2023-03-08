416ffb5cbb7586727d072bdd2e84c94cf0c231ce
EstebanRiso <steveriso.2000@gmail.com>
2023-03-08 11:45:09 -0300

    intentando arreglar las imagenes

--- next.config.js
+++ next.config.js
@@ -3,4 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["localhost"],
+    // next line is not required
+    path: 'http://localhost:8080/images'
+  }
+};
