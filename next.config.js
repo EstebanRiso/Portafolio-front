ca9d8a0734160e2959bf239c69322b3599696bc7
EstebanRiso <steveriso.2000@gmail.com>
2023-03-07 22:31:24 -0300

    modificando next_config para que lea la carpeta public

--- next.config.js
+++ next.config.js
@@ -3,4 +3,17 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+      };
+    }
+    return config;
+  },
+  images: {
+    loader: 'imgix',
+    path: '/',
+  },
+};
